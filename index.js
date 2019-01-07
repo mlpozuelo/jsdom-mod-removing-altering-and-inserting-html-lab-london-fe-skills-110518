44556f40f49021f024abcb65533b424e4bd3eaf2
Maria Lopez Pozuelo <mlpozuelo@google.com>
2019-01-07 19:24:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
-
+main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+var title1 = document.createElement('h1#victory')/* Create elemet*/
+title1.innerHTML='Maria is the champion!'
+document.body.appendChild(title1) /* Use it - append to document */
